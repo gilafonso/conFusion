331e6f4e5e3516bd2f3543ac7abf04ce39c8ee47
Gil Afonso <gil.lomba.afonso@gmail.com>
2020-10-15 12:13:01 +0100

    implement react animation components

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -3,18 +3,24 @@ import { Card, CardImg, CardBody, CardText, CardTitle, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Loading } from './LoadingComponent';
+import { FadeTransform, Fade, Stagger } from 'react-animation-components';
 
     function RenderDish({dish}) {
         if (dish != null) {
             return (
                 <div className='col-12 col-md-5 m-1'>
-                    <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
-                        <CardBody>
-                            <CardTitle>{dish.name}</CardTitle>
-                            <CardText>{dish.description}</CardText>
-                        </CardBody>
-                    </Card>
+                    <FadeTransform in 
+                        transformProps={{
+                            exitTransform: 'scale(0.5) translateY(-50%)'
+                        }}>
+                        <Card>
+                            <CardImg width="100%" src={dish.image} alt={dish.name} />
+                            <CardBody>
+                                <CardTitle>{dish.name}</CardTitle>
+                                <CardText>{dish.description}</CardText>
+                            </CardBody>
+                        </Card>
+                    </FadeTransform>
                 </div>
             )
         }
@@ -43,7 +49,11 @@ import { Loading } from './LoadingComponent';
             <div className='col-12 col-md-5 m-1'>
                 <h4> Comments </h4>
                 <ul className='list-unstyled'>
-                    {coments}
+                    <Stagger in>
+                        <Fade in>
+                            {coments}
+                        </Fade>
+                    </Stagger>
                 </ul>
                 <CommentForm dishId={dishId} addComment={addComment} />
             </div>
